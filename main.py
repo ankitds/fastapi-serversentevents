6f329a61c62777c17546a29495c5eac4ccfa14cb
Ankit S <ankit@designstring.com>
2022-10-13 18:38:10 +0530

    set gunicorn with uvicorn

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from typing import List
 
 app = FastAPI()
 
-# def get_balances(Request, chain, address):
-#     url = f"https://v2.unifront.io/v2/{chain}/address/{address}/balances"
-#     response = requests.get(url = url)
-#     data = response.json()
-#     return data
+def get_balances(Request, chain, address):
+    url = f"https://v2.unifront.io/v2/{chain}/address/{address}/balances"
+    response = requests.get(url = url)
+    data = response.json()
+    return data
 
 
 @app.get("/")
@@ -22,40 +22,40 @@ async def root(request: Request):
     response = {"Page" : "home_page"}
     return response
 
-# Balance_details = {}
-# #API to send the success response
-# @app.post("/response/")
-# async def root(request: Request):
-#     response = {}
-#     data = await request.json()
-#     chain,address = data.get("chain"), data.get("address")
-#     response['data'] = {"chain" : chain, "address" : address}
-#     response['status'] = "200 OK"
-#     Balance = get_balances(Request, chain, address)
+Balance_details = {}
+#API to send the success response
+@app.post("/response/")
+async def root(request: Request):
+    response = {}
+    data = await request.json()
+    chain,address = data.get("chain"), data.get("address")
+    response['data'] = {"chain" : chain, "address" : address}
+    response['status'] = "200 OK"
+    Balance = get_balances(Request, chain, address)
     
-#     if Balance:
-#         print("rresult",Balance)
-#         Balance_details['balance'] = Balance
-#         events = message_stream(Request)
-#     return response
+    if Balance:
+        print("rresult",Balance)
+        Balance_details['balance'] = Balance
+        events = message_stream(Request)
+    return response
 
 
-# STREAM_DELAY = 2  # second
-# RETRY_TIMEOUT = 1500 
+STREAM_DELAY = 2  # second
+RETRY_TIMEOUT = 1500 
 
 
-# #API to generate events and streamed data
-# @app.get('/stream/data/')
-# async def message_stream(request: Request):
-#     async def event_generator():
-#         # while True:
-#             # if await request.is_disconnected():
-#             #     break  
-#         if Balance_details:
-#             yield {
-#                     "data": Balance_details
-#             }
-#         await asyncio.sleep(STREAM_DELAY)
+#API to generate events and streamed data
+@app.get('/stream/data/')
+async def message_stream(request: Request):
+    async def event_generator():
+        # while True:
+            # if await request.is_disconnected():
+            #     break  
+        if Balance_details:
+            yield {
+                    "data": Balance_details
+            }
+        await asyncio.sleep(STREAM_DELAY)
 
-#     return EventSourceResponse(event_generator())
+    return EventSourceResponse(event_generator())
 
